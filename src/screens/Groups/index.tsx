a6009577d0bb1ed739839fc2c764aefe524e77ea
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-15 19:16:34 -0300

    distribuindo os arquivos.

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export function Groups(){
+    return (
+        <Container>
+            <Title> Romildo Jr. </Title>
+        </Container>
+    )
+}
